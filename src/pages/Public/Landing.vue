430c3cecade535d517cb9cc5a6a0182f8a8025f4
Onyejiaka Samuel Ikenna <onyejiaka.samuel@gmail.com>
2026-02-10 23:53:03 +0100

    fixed content

--- src/pages/Public/Landing.vue
+++ src/pages/Public/Landing.vue
@@ -28,11 +28,11 @@ const features = [
         <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+        <!-- <div class="hidden sm:mb-8 sm:flex sm:justify-center">
           <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 dark:text-gray-400 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20 dark:hover:ring-gray-100/20 transition-all">
             Announcing our new Series A funding. <a href="#" class="font-semibold text-primary-600"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>
           </div>
-        </div>
+        </div> -->
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">Modern Treasury Management for the Digital Age</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
